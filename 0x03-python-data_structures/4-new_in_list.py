635956d0c30edc09a87ec6fcc880c167153fd7ae
Hache <943@holbertonschool.com>
2019-09-17 15:19:31 -0500

    fix2Task4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
-   cpy_list = my_list
-   leng = len(cpy_list)
-   
-   for i in cpy_list:
-      if idx < 0:
-         return(cpy_list)
-      elif idx > leng:
-         return(cpy_list)
-      elif i == cpy_list[idx]:
-         cpy_list[idx] = element
-         return(my_list)
+    cpy_list = my_list.copy()
+    leng = len(cpy_list)
+
+    for i in cpy_list:
+        if idx < 0:
+            return(cpy_list)
+        elif idx > leng:
+            return(cpy_list)
+        elif i == cpy_list[idx]:
+            cpy_list[idx] = element
+            return(cpy_list)
