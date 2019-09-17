2834a8ff2b8243a2e0e02723002db5395b622024
Hache <943@holbertonschool.com>
2019-09-17 13:57:41 -0500

    fix3Task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,11 +3,9 @@ def element_at(my_list, idx):
     leng = len(my_list)
 
     for i in my_list:
-        if my_list == 0:
-            break
         if idx < 0:
             return(None)
-        elif idx > leng:
+        elif idx > leng - 1:
             return(None)
         elif i == my_list[idx]:
             return(my_list[idx])
