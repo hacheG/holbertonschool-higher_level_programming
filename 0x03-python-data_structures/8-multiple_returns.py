ac2cee8fbe86f1ec082e0182009c747de07d9ef8
Hache <943@holbertonschool.com>
2019-09-17 19:47:54 -0500

    fix3Task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return(None, None)
     leng = (len(sentence))
+    if sentence is None:
+        return(leng, None)
+
     fst_lettr = sentence[0]
     return(leng, fst_lettr)
