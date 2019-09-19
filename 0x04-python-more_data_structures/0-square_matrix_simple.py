abb9024ce260cfc211fa8cac5244711b70bde2df
Hache <943@holbertonschool.com>
2019-09-19 14:10:54 -0500

    fix1Task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     leng = len(new_matrix)
 
     for i in range(leng):
-        new_matrix[i] = list(map(lambda x: x*x,new_matrix[i]))
+        new_matrix[i] = list(map(lambda x: x*x, new_matrix[i]))
 
     return(new_matrix)
