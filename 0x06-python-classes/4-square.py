0fe9d2a7320bfbb289bc83da9cc344ba79f5c034
hacheG <943@holbertonschool.com>
2019-10-01 21:46:42 +0000

    fix3Task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,9 +6,6 @@ class Square():
     def __init__(self, size=0):
         self.__size = size
 
-    def area(self):
-        return (self.__size * self.__size)
-
     @property
     def size(self):
         return self.__size
@@ -21,3 +18,6 @@ class Square():
             raise ValueError("size must be >= 0")
 
         self.__size = x
+
+    def area(self):
+        return (self.__size * self.__size)
