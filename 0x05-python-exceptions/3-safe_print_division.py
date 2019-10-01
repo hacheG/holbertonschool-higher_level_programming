5b8a213a437131349dc4c7db88f63e7e54d0ae97
hacheG <943@holbertonschool.com>
2019-09-30 21:19:51 -0500

    fix1task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	divRes = None
-	try:
-		divRes = a /b
-	except ZeroDivisionError:
-		return None
-	finally:
-		print("{} {}".format("Inside result:", divRes))
-	return divRes
+    divRes = None
+    try:
+        divRes = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("{} {}".format("Inside result:", divRes))
+    return divRes
