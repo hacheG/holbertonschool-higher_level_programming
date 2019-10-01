24f3aba5f9b97cae4791dd2b13f6fe4877b8bf2d
hacheG <943@holbertonschool.com>
2019-10-01 19:50:39 +0000

    task4_5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class Square():
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, x):
+        if not str(x).isdigit():
+            raise TypeError("size must be an integer")
+        if x < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = x
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
