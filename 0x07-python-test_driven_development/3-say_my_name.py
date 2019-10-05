09e4be80cd6362214256d66dc7d77ef1bd065b73
hacheG <943@holbertonschool.com>
2019-10-04 23:48:08 -0500

    fixTask3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+    Function that prints the name of a person
+"""
+
+
 def say_my_name(first_name, last_name=""):
+	  """
+    Args:
+        first_name: string
+        last_name: string
+    Raises:
+        TypeError:
+                 * first_name must be a string
+                 * last_name must be a string
+    Returns:
+        print the name and last name
+    """
 	msnErr1 = "first_name must be a string"
 	msnErr2 = "last_name must be a string"
 
