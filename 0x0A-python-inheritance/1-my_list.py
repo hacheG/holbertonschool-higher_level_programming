82d2473713aa7ebc794743e0d2404504cd2b7b15
hacheG <943@holbertonschool.com>
2019-10-15 22:31:11 -0500

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function in python
+"""
+
+
+class MyList(list):
+    """
+    begin class - MyList
+    """
+    def print_sorted(self):
+        """
+        begin function - print_sorted
+        """
+        print("{}".format(sorted(self)))
