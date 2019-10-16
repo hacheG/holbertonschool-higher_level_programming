defe8f8e5bffc4fa29ca019c8dcae0aeabc45a8b
hacheG <943@holbertonschool.com>
2019-10-15 22:49:59 -0500

    add

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+begin program - python
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    begin function - is_kind_of_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
