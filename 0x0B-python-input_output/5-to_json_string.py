c54a1f8866a981b16e7fc331e73336664d0070e6
hacheG <943@holbertonschool.com>
2019-10-16 21:43:34 -0500

    add

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Begin program - python
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    begin function - to_json_string
+    """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Begin program - python
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    begin function - from_json_string
+    """
+    return json.loads(my_str)
