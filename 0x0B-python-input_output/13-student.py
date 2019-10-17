a1daf6afa6a5c7efd55dbecfce1480945836590a
hacheG <943@holbertonschool.com>
2019-10-16 22:14:51 -0500

    add

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Begin program - python"""
+
+
+class Student:
+    """Begin program - python"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """To JSON"""
+        if attrs is None:
+            return self.__dict__
+        d = {}
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            for el in attrs:
+                if el in self.__dict__:
+                    d[el] = self.__dict__[el]
+            return d
+
+    def reload_from_json(self, json):
+        """Reloads JSON file"""
+        for keys in json:
+            self.__dict__[keys] = json[keys]
