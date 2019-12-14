dbdf3dfc70b7d967ba5955eac7b8748ef6dca23e
hacheG <943@holbertonschool.com>
2019-12-14 04:04:11 +0000

    [FIX] pep8 style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-#mode christmas
+# mode christmas
 
 import MySQLdb
-import sys	
+import sys
 
 if __name__ == '__main__':
 
-	db_host = 'localhost'
-	usuario = sys.argv[1]
-	clave = sys.argv[2]
-	base_de_datos = sys.argv[3]
-	db = MySQLdb.connect(host=db_host, user=usuario, password=clave, database=base_de_datos, port=3306)
-#	conexion = mysql.connector.connect(**dbConnect)
+    db_host = 'localhost'
+    usuario = sys.argv[1]
+    clave = sys.argv[2]
+    base_de_datos = sys.argv[3]
+    db = MySQLdb.connect(host=db_host, user=usuario, password=clave,
+                         database=base_de_datos, port=3306)
 
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states")
-#	mi_query = "SELECT * FROM state"
-#	cursor.execute(mi_query)
-	rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-	for data in rows:
-		print (data)
-#	cur.close()
-#	db.cose()
+    for data in rows:
+        print(data)
+    cur.close()
+    db.cose()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,18 @@
-khgjgfjhgfj
+#!/usr/bin/python3
+#winter is here
+import MySQLdb
+import sys
+ 
+if __name__ == '__main__':
+	db_host = 'localhost'
+	usuario = sys.argv[1]
+	clave = sys.argv[2]
+	base_de_datos = sys.argv[3]
+
+	db = MySQLdb.connect(host=db_host, user=usuario, password=clave, database=base_de_datos, port=3306)
+	cur = db.cursor()
+	cur.execute("SELECT * FROM states")
+	rows = cur.fetchall()
+
+	for data in rows:
+		print (data)
