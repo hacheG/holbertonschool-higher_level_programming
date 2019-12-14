66ad3bf13dda6035b77598325ef8f46f8e4b612a
hacheG <943@holbertonschool.com>
2019-12-14 15:01:03 +0000

    [FEATURE] task12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,23 @@
-jytfytdh
+#!/usr/bin/python3
+# qwerty-qwerty-qwerty
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_mex = session.query(State).filter_by(id=2).first()
+    try:
+        new_mex.name = "New Mexico"
+    except Exception:
+        pass
+    session.commit()
+    session.close()
