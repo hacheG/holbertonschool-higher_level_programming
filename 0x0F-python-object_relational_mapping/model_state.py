d18549803952c169f02835f66d43607f776bca95
hacheG <943@holbertonschool.com>
2019-12-14 12:57:56 +0000

    [FEATURE] add gitignore & task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,2 +1,13 @@
-khfjhfhg
+#!/usr/bin/python3
+# bla-bla-bla
 
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
