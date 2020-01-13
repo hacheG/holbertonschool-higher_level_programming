2def8dfd251f755d897d2d66a20f4c62641d5a99
hacheG <943@holbertonschool.com>
2020-01-13 18:41:11 -0500

    [FEATURE] add task_3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""close to you"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print("Error code:", err.code)
