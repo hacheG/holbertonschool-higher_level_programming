7a10afa1958a6dc4528383086bfc8afcf41dad7f
hacheG <943@holbertonschool.com>
2020-01-13 17:51:01 -0500

    [FEATURE] add task_2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""bla bla bla"""
+
+from urllib import request, parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    theMail = argv[2]
+    data = parse.urlencode({'email': theMail}).encode()
+    
+    with request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
