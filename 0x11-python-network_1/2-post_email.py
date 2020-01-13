304dbd7a08d21e7dee20e2c08fd053e809b9978c
hacheG <943@holbertonschool.com>
2020-01-13 17:58:38 -0500

    [FIX] quite blanck line

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     url = argv[1]
     theMail = argv[2]
     data = parse.urlencode({'email': theMail}).encode()
-    
+
     with request.urlopen(url, data) as response:
         print(response.read().decode('utf-8'))
