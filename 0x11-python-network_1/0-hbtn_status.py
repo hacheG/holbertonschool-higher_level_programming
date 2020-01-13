d6c4eb0635bebdd183e099ddc4f360733e4bfa6d
hacheG <943@holbertonschool.com>
2020-01-13 15:30:44 -0500

    [FEATURE] addTask0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""painnnn more paiiinnnn"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as openUrl:
+        page = openUrl.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf-8')))
