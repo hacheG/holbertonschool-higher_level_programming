c993f0825773f8ed72877e89e21d420e3cdf87bd
hacheG <943@holbertonschool.com>
2020-02-05 01:27:07 -0500

    [FIX] mas standard style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (i in arr) {
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const i in arr) {
   console.log(arr[i])
 }
