50af1e48a98c0a8a94192c4e8a0160d2458f4902
hacheG <943@holbertonschool.com>
2020-02-10 21:39:46 -0500

    fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,13 +5,14 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+  
   print () {
     for (let i = 0; i < this.height; i++) {
       let ex = '';
       for (let j = 0; j < this.width; j++) {
         ex = ex + 'X';
       }
-    console.log(ex);
+      console.log(ex);
     }
   }
 };
