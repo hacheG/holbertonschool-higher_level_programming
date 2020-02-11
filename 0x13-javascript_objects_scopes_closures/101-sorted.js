13a75cbc2645e35a058ebd47f6c763454c5e1122
hacheG <943@holbertonschool.com>
2020-02-11 00:09:59 -0500

    w

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const listk = Object.keys(dict);
+const newdict = {};
+for (let con = 0; con < listk.length; con++) {
+  const nkey = dict[listk[con]];
+  if (newdict[nkey]) {
+    newdict[nkey].push(listk[con]);
+  } else {
+    newdict[nkey] = [listk[con]];
+  }
+}
+console.log(newdict);
