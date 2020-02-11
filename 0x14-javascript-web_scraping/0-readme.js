cd078fc6b9e22789e7c8e73d9b8556f5b8f16221
hacheG <943@holbertonschool.com>
2020-02-11 17:25:20 +0000

    [FIX] console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], { encoding: 'utf8' }, (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
