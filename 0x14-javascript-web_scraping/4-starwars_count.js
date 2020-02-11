e009583e738d4f557d789e3b0230e0d3e985471b
hacheG <943@holbertonschool.com>
2020-02-11 22:33:55 +0000

    [FEATURE] add task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+request(url, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  let count = 0;
+  for (const q of body.results) {
+    for (const w of q.characters) {
+      if (w === 'https://swapi.co/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
