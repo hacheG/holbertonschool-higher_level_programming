180b038df7a32a2bdfb60ea652f422f6449d4202
hacheG <943@holbertonschool.com>
2020-02-12 04:58:39 +0000

    casi que no

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  let count = 0;
+  let counTen = 0;
+  const a = {};
+  for (let i = 0; i < body.length; i++) {
+    counTen += 1;
+    if (body[i].completed === true) {
+      count += 1;
+      a[body[i].userId] = count;
+    }
+    if (counTen === 20) {
+      count = 0;
+      counTen = 0;
+    }
+  }
+  console.log(a);
+});
